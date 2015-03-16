a94b1f63d3fdcf4bb229ffdf857e2f2cd6fbafcc
Dawid Ciężarkiewicz <dpc@ucore.info>
2015-03-15 22:39:08 -0700

    Initial commit.

---
+++ build.rs
@@ -0,0 +1,21 @@
+#![feature(old_io, old_path)]
+
+// Generate the git version string.
+
+use std::old_io::process::{Command, ProcessOutput};
+use std::old_io::{File};
+
+pub fn main() {
+    match Command::new("git")
+        .arg("clone").arg("https://github.com/rust-lang/rust.git").output() {
+            Ok(ProcessOutput { status: exit, output: out, error: err }) => {
+                if exit.success() {
+                } else {
+                    println!("Error getting rustc version: {}", String::from_utf8(err).unwrap());
+                }
+            }
+            Err(err) => {
+                println!("Error cloning rustc git repo: {}", err);
+            }
+        }
+}
